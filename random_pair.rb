246366f5c733ee94ad7c665de25d22289acea20a
Ricky Collins <richardcollins52317@gmail.com>
2018-07-22 15:34:34 -0400

    misunderstood how the pairs where being randomized

--- random_pair.rb
+++ random_pair.rb
@@ -8,44 +8,54 @@ def pair(names)
 
     list = []
     first_names = []
-    last_names = []
+    second_names = []
 
-    names.each do |n|
-        name = n.split(" ")
-        first_names << name[0]
-        if name.length > 1
-            last_names << name[1]
+    names.shuffle!
+
+    names.each_with_index do |v, i|
+        if i % 2 == 0
+            first_names << v
+        else
+            second_names << v
         end
     end
 
+    # names.each do |v|
+    #     name = v.split(" ")
+    #     first_names << name[0]
+    #     if name.length > 1
+    #         second_names << name[1]
+    #     end
+    # end
+
     first_names.shuffle!
-    last_names.shuffle!
+    second_names.shuffle!
 
     x = first_names.length
-    y = last_names.length
+    y = second_names.length
 
     if x > y && (x - y) % 2 == 0
         count = (x - y) / 2
         while count > 0 
             temp = first_names.pop
-            last_names << temp
+            second_names << temp
             count -=1
         end
     elsif x > y && (x - y) % 2 != 0
         count = (x - y) / 2
         while count > 0
             temp = first_names.pop
-            last_names << temp
+            second_names << temp
             count -=1
         end
     end
 
-    last_names.each_with_index do |v, i|
+    second_names.each_with_index do |v, i|
         list << [first_names[i], v]
     end
 
     a = first_names.length
-    b = last_names.length
+    b = second_names.length
     
     if a == (b + 1)
         temp = first_names.pop
@@ -53,8 +63,7 @@ def pair(names)
     end
 
     if list[-1].is_a?(String)
-        oddball = ""
-        oddball += list[-1]
+        oddball = list[-1]
         list.delete_at(-1)
         list[-1] << oddball
     end
