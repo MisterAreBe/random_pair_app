433b703438016089ce0f952c72ab9b0f67db0c8f
Ricky Collins <richardcollins52317@gmail.com>
2018-07-28 14:17:40 -0400

    I think I added the js that was required.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,6 @@
 function loadBox() {
     var box = document.createElement("input");
-    var add = document.getElementById("boxes");
+    var add = document.getElementById("box");
     box.setAttribute("type", "text");
     box.setAttribute("name", "names[]");
     box.setAttribute("class", "name_box");
@@ -9,16 +9,26 @@ function loadBox() {
 }
 
 function addBox() {
-    var box = document.createElement("input");
     var add = document.getElementById("boxes");
-    box.setAttribute("type", "text");
-    box.setAttribute("name", "names[]");
-    box.setAttribute("class", "name_box");
-    box.setAttribute("placeholder", "Enter a name here");
-    add.appendChild(box);
+    var numBox = document.getElementById("numBoxes").value;
+    for (i=1;i<numBox;i++){
+        var box = document.createElement("input");
+        box.setAttribute("type", "text");
+        box.setAttribute("name", "names[]");
+        box.setAttribute("class", "name_box");
+        box.setAttribute("placeholder", "Enter a name here");
+        add.appendChild(box);
+    };
 }
 
 function removeBox(){
     var badBox = document.getElementById("boxes");
     badBox.removeChild(badBox.lastChild);
 }
+
+function removeAllBoxes(){
+    var boxes = document.getElementById("boxes");
+    while(boxes.hasChildNodes()){
+        boxes.removeChild(boxes.lastChild);
+    };
+};
